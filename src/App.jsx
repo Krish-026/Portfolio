37a33195f37d55ee57b71eb7161e0d70c4e2518a
Krishna <kpratapsingh026@gmail.com>
2024-02-09 22:18:39 +0530

    Change in file name of Parallax

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from './components/contact/Contact';
 import Cursor from './components/cursor/Cursor';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
-import Parallax from './components/parallax/parallax';
+import Parallax from './components/parallax/Parallax';
 import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 const App = () => {
